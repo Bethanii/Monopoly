ae6b408ebbb2a09668d909c5c25e3ece6889cd97
bethanii <bethanihampton@gmail.com>
2024-03-28 08:26:02 -0500

    add dice UI

--- Gameboard.Designer.cs
+++ Gameboard.Designer.cs
@@ -33,8 +33,15 @@ namespace MonopolyGame
             sellButton = new Button();
             nextTurnButton = new Button();
             getOutOfJailFreeCards = new PictureBox();
+            diceRoll1 = new PictureBox();
+            diceRoll2 = new PictureBox();
+            groupBox1 = new GroupBox();
+            rollDiceButton = new Button();
             ((System.ComponentModel.ISupportInitialize)gameBoardImage).BeginInit();
             ((System.ComponentModel.ISupportInitialize)getOutOfJailFreeCards).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)diceRoll1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)diceRoll2).BeginInit();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // gameBoardImage
@@ -43,11 +50,11 @@ namespace MonopolyGame
             gameBoardImage.BackgroundImage = (Image)resources.GetObject("gameBoardImage.BackgroundImage");
             gameBoardImage.BackgroundImageLayout = ImageLayout.Stretch;
             gameBoardImage.Cursor = Cursors.No;
-            gameBoardImage.Location = new Point(289, 12);
+            gameBoardImage.Location = new Point(292, 12);
             gameBoardImage.MaximumSize = new Size(840, 840);
             gameBoardImage.MinimumSize = new Size(565, 565);
             gameBoardImage.Name = "gameBoardImage";
-            gameBoardImage.Size = new Size(565, 565);
+            gameBoardImage.Size = new Size(601, 569);
             gameBoardImage.TabIndex = 2;
             gameBoardImage.TabStop = false;
             gameBoardImage.Click += gameBoardImage_Click;
@@ -58,9 +65,9 @@ namespace MonopolyGame
             propertiesGroupBox.AutoSizeMode = AutoSizeMode.GrowAndShrink;
             propertiesGroupBox.BackColor = Color.AliceBlue;
             propertiesGroupBox.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
-            propertiesGroupBox.Location = new Point(884, 140);
+            propertiesGroupBox.Location = new Point(939, 146);
             propertiesGroupBox.Name = "propertiesGroupBox";
-            propertiesGroupBox.Size = new Size(328, 422);
+            propertiesGroupBox.Size = new Size(294, 422);
             propertiesGroupBox.TabIndex = 8;
             propertiesGroupBox.TabStop = false;
             propertiesGroupBox.Text = "Properties";
@@ -68,25 +75,28 @@ namespace MonopolyGame
             // label2
             // 
             label2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            label2.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(884, 41);
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 15F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = SystemColors.ButtonHighlight;
+            label2.Location = new Point(939, 32);
             label2.Name = "label2";
-            label2.Size = new Size(82, 28);
+            label2.Size = new Size(91, 28);
             label2.TabIndex = 10;
             label2.Text = "Balance:";
+            label2.Click += label2_Click_1;
             // 
             // textBox1
             // 
             textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            textBox1.Location = new Point(884, 72);
+            textBox1.Location = new Point(939, 72);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(328, 45);
+            textBox1.Size = new Size(294, 45);
             textBox1.TabIndex = 11;
+            textBox1.TextChanged += textBox1_TextChanged_1;
             // 
             // buyButton
             // 
-            buyButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             buyButton.AutoSizeMode = AutoSizeMode.GrowAndShrink;
             buyButton.BackColor = Color.MediumSpringGreen;
             buyButton.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
@@ -101,18 +111,19 @@ namespace MonopolyGame
             // label1
             // 
             label1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(28, 397);
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(12, 430);
             label1.MaximumSize = new Size(208, 23);
             label1.Name = "label1";
-            label1.Size = new Size(208, 23);
+            label1.Size = new Size(208, 19);
             label1.TabIndex = 13;
             label1.Text = "Get Out of Jail Free Cards:";
             label1.Click += label1_Click_1;
             // 
             // sellButton
             // 
-            sellButton.Anchor = AnchorStyles.Left;
             sellButton.AutoSizeMode = AutoSizeMode.GrowAndShrink;
             sellButton.BackColor = Color.Yellow;
             sellButton.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
@@ -126,7 +137,6 @@ namespace MonopolyGame
             // 
             // nextTurnButton
             // 
-            nextTurnButton.Anchor = AnchorStyles.Left;
             nextTurnButton.AutoSizeMode = AutoSizeMode.GrowAndShrink;
             nextTurnButton.BackColor = Color.LightSkyBlue;
             nextTurnButton.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
@@ -143,12 +153,65 @@ namespace MonopolyGame
             getOutOfJailFreeCards.Anchor = AnchorStyles.Left;
             getOutOfJailFreeCards.BackgroundImage = (Image)resources.GetObject("getOutOfJailFreeCards.BackgroundImage");
             getOutOfJailFreeCards.BackgroundImageLayout = ImageLayout.Stretch;
-            getOutOfJailFreeCards.Location = new Point(28, 423);
-            getOutOfJailFreeCards.MaximumSize = new Size(208, 216);
+            getOutOfJailFreeCards.Location = new Point(12, 465);
+            getOutOfJailFreeCards.MaximumSize = new Size(218, 226);
             getOutOfJailFreeCards.Name = "getOutOfJailFreeCards";
-            getOutOfJailFreeCards.Size = new Size(208, 116);
+            getOutOfJailFreeCards.Size = new Size(218, 116);
             getOutOfJailFreeCards.TabIndex = 17;
             getOutOfJailFreeCards.TabStop = false;
+            // 
+            // diceRoll1
+            // 
+            diceRoll1.BackColor = Color.Transparent;
+            diceRoll1.BackgroundImage = (Image)resources.GetObject("diceRoll1.BackgroundImage");
+            diceRoll1.BackgroundImageLayout = ImageLayout.Stretch;
+            diceRoll1.Location = new Point(26, 24);
+            diceRoll1.Name = "diceRoll1";
+            diceRoll1.Size = new Size(83, 79);
+            diceRoll1.TabIndex = 18;
+            diceRoll1.TabStop = false;
+            diceRoll1.Click += diceRoll1_Click;
+            // 
+            // diceRoll2
+            // 
+            diceRoll2.BackColor = Color.Transparent;
+            diceRoll2.BackgroundImage = (Image)resources.GetObject("diceRoll2.BackgroundImage");
+            diceRoll2.BackgroundImageLayout = ImageLayout.Stretch;
+            diceRoll2.Location = new Point(115, 24);
+            diceRoll2.Name = "diceRoll2";
+            diceRoll2.Size = new Size(83, 79);
+            diceRoll2.TabIndex = 19;
+            diceRoll2.TabStop = false;
+            diceRoll2.Click += diceRoll2_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Anchor = AnchorStyles.Left;
+            groupBox1.BackColor = Color.Transparent;
+            groupBox1.Controls.Add(diceRoll2);
+            groupBox1.Controls.Add(diceRoll1);
+            groupBox1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            groupBox1.ForeColor = SystemColors.ButtonFace;
+            groupBox1.Location = new Point(12, 243);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(224, 119);
+            groupBox1.TabIndex = 20;
+            groupBox1.TabStop = false;
+            // 
+            // rollDiceButton
+            // 
+            rollDiceButton.Anchor = AnchorStyles.Left;
+            rollDiceButton.BackColor = Color.Tomato;
+            rollDiceButton.BackgroundImageLayout = ImageLayout.Stretch;
+            rollDiceButton.Font = new Font("Segoe UI", 15F, FontStyle.Bold, GraphicsUnit.Point);
+            rollDiceButton.ForeColor = SystemColors.ControlText;
+            rollDiceButton.Location = new Point(56, 352);
+            rollDiceButton.Name = "rollDiceButton";
+            rollDiceButton.Size = new Size(131, 43);
+            rollDiceButton.TabIndex = 21;
+            rollDiceButton.Text = "Roll Dice";
+            rollDiceButton.UseVisualStyleBackColor = false;
+            rollDiceButton.Click += button1_Click_2;
             // 
             // Gameboard
             // 
@@ -157,6 +220,8 @@ namespace MonopolyGame
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(1245, 654);
+            Controls.Add(rollDiceButton);
+            Controls.Add(groupBox1);
             Controls.Add(getOutOfJailFreeCards);
             Controls.Add(nextTurnButton);
             Controls.Add(sellButton);
@@ -170,6 +235,9 @@ namespace MonopolyGame
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)gameBoardImage).EndInit();
             ((System.ComponentModel.ISupportInitialize)getOutOfJailFreeCards).EndInit();
+            ((System.ComponentModel.ISupportInitialize)diceRoll1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)diceRoll2).EndInit();
+            groupBox1.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -184,5 +252,9 @@ namespace MonopolyGame
         private Button sellButton;
         private Button nextTurnButton;
         private PictureBox getOutOfJailFreeCards;
+        private PictureBox diceRoll1;
+        private PictureBox diceRoll2;
+        private GroupBox groupBox1;
+        private Button rollDiceButton;
     }
 }
--- Gameboard.cs
+++ Gameboard.cs
@@ -55,5 +55,28 @@ namespace MonopolyGame
         {
 
         }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+        }
+
+        private void diceRoll2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void diceRoll1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+
+        }
     }
 }
