249553e6b14cc14334b838687df2c78af2b2df34
Andrew <andrewjgrant45@gmail.com>
2024-03-29 18:39:43 -0500

    Added start menu form

--- Program.cs
+++ Program.cs
@@ -11,7 +11,8 @@ namespace MonopolyGame
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Gameboard());
+            //changed to StartMenu for testing
+            Application.Run(new StartMenu());
         }
     }
 }
