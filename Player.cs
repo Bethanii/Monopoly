f840eadf593c39980f022592d7ced30867d73228
Andrew <andrewjgrant45@gmail.com>
2024-03-28 21:41:39 -0500

    Adding player class

---
+++ Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonopolyGame
+{
+    internal class Player
+    {
+        private string name;
+        private string piece;
+        private string color;
+        private int boardPosition;
+        private int moneyBalance;
+        private List<String> properties = new List<string>();
+        private int inJailCounter;
+
+        public Player(string name, string piece, string color, int boardPosition, int moneyBalance, int inJailCounter)
+        {
+            this.name = name;
+            this.piece = piece;
+            this.color = color;
+            this.boardPosition = boardPosition;
+            this.moneyBalance = moneyBalance;
+            this.inJailCounter = inJailCounter;
+        }
+    }
+}
