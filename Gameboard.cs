6d9c8c4abe8a694e1b33ef97f1220be6a5cfbbc6
Andrew <andrewjgrant45@gmail.com>
2024-04-23 21:28:59 -0500

    Moved property dictionary to its own class and added railroads and utilities. Added Railroad, Street, and Utility subclasses to Property class. changed players money balance when they purchase properties.

--- Gameboard.cs
+++ Gameboard.cs
@@ -10,9 +10,9 @@ namespace MonopolyGame
         private List<Player> players;
         private int currentPlayerIndex = 0;
         private Dictionary<Player, PictureBox> playerPieces = new Dictionary<Player, PictureBox>();
-        private Dictionary<int, Property> boardPositionToPropertyMap;
         private Dictionary<Panel, Property> panelToPropertyMap = new Dictionary<Panel, Property>();
         private Panel lastClickedPanel;
+        private PropertyList propertyList = new PropertyList();
 
         private int rollCount = 0;
         int doublesCount = 0;
@@ -230,7 +230,10 @@ namespace MonopolyGame
 
                 Player currentPlayer = players[currentPlayerIndex];
                 gameplay.movePiece(currentPlayer, total, playerPieces, spaces);
+                //updates money display if passing go
+                balanceTextBox.Text = "$" + players[0].getMoneyBalance();
 
+                //checks for doubles
                 if (dice1 == dice2)
                 {
                     doublesCount++;
@@ -318,16 +321,27 @@ namespace MonopolyGame
                 Player currentPlayer = players[currentPlayerIndex];
                 int currentBoardPosition = currentPlayer.getBoardPosition();
 
-                if (boardPositionToPropertyMap.TryGetValue(currentBoardPosition, out Property currentProperty))
+                if (propertyList.getProperties().TryGetValue(currentBoardPosition, out Property currentProperty))
                 {
                     if (currentProperty.getOwner() == null)
                     {
-                        propertyNameLabel.Text = currentProperty.getName();
-                        propertyPanel.BringToFront();
-                        boardPositionToPropertyMap[currentBoardPosition] = currentProperty;
-                        currentPlayer.addProperties(currentProperty);
-                        updateCurrentPlayerProperties();
-                        AdjustGroupBoxSize();
+                        if (currentPlayer.getMoneyBalance() > currentProperty.getCost())
+                        {
+                            propertyNameLabel.Text = currentProperty.getName();
+                            propertyPanel.BringToFront();
+                            propertyList.getProperties()[currentBoardPosition] = currentProperty;
+                            currentPlayer.addProperties(currentProperty);
+                            updateCurrentPlayerProperties();
+                            AdjustGroupBoxSize();
+
+                            //Charge player cost of prioperty
+                            currentPlayer.setMoneyBalance(currentPlayer.getMoneyBalance() - currentProperty.getCost());
+                            balanceTextBox.Text = "$" + players[0].getMoneyBalance();
+                        }
+                        else
+                        {
+                            MessageBox.Show("You can not afford this property.", "Property Too Expensive", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
                     }
                     else
                     {
@@ -376,14 +390,14 @@ namespace MonopolyGame
         {
             var colorMap = new Dictionary<string, Color>
             {
-                { "Brown", Color.SaddleBrown },
-                { "Light Blue", Color.LightSkyBlue },
-                { "Magenta", Color.MediumVioletRed },
-                { "Orange", Color.Orange },
-                { "Red", Color.Red },
-                { "Yellow", Color.Gold },
-                { "Green", Color.MediumSeaGreen },
-                { "Dark Blue", Color.DodgerBlue }
+                { "brown", Color.SaddleBrown },
+                { "lightBlue", Color.LightSkyBlue },
+                { "pink", Color.MediumVioletRed },
+                { "orange", Color.Orange },
+                { "red", Color.Red },
+                { "yellow", Color.Gold },
+                { "green", Color.MediumSeaGreen },
+                { "darkBlue", Color.DodgerBlue }
             };
 
             if (colorMap.TryGetValue(colorGroup, out Color color))
@@ -395,89 +409,7 @@ namespace MonopolyGame
 
         private void initializeProperties()
         {
-            boardPositionToPropertyMap = new Dictionary<int, Property>();
-
-            //hard coding rent values for now
-            //------------- Brown ------------------
-            Property mediterraneanAvenue = new Property("Mediterranean Avenue", "Brown", 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
-            boardPositionToPropertyMap.Add(mediterraneanAvenue.getBoardPosition(), mediterraneanAvenue);
-
-            Property balticAvenue = new Property("Baltic Avenue", "Brown", 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3);
-            boardPositionToPropertyMap.Add(balticAvenue.getBoardPosition(), balticAvenue);
-
-
-            //------------- Light Blue------------------
-            Property orientalAvenue = new Property("Oriental Avenue", "Light Blue", 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6);
-            boardPositionToPropertyMap.Add(orientalAvenue.getBoardPosition(), orientalAvenue);
-
-            Property vermontAvenue = new Property("Vermont Avenue", "Light Blue", 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8);
-            boardPositionToPropertyMap.Add(vermontAvenue.getBoardPosition(), vermontAvenue);
-
-            Property connecticutAvenue = new Property("Connecticut Avenue", "Light Blue", 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9);
-            boardPositionToPropertyMap.Add(connecticutAvenue.getBoardPosition(), connecticutAvenue);
-
-
-            //------------- Magenta ------------------
-            Property stCharlesPlace = new Property("St. Charles Place", "Magenta", 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 11);
-            boardPositionToPropertyMap.Add(stCharlesPlace.getBoardPosition(), stCharlesPlace);
-
-            Property statesAve = new Property("States Avenue", "Magenta", 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 13);
-            boardPositionToPropertyMap.Add(statesAve.getBoardPosition(), statesAve);
-
-            Property virginiaAve = new Property("Virginia Avenue", "Magenta", 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 14);
-            boardPositionToPropertyMap.Add(virginiaAve.getBoardPosition(), virginiaAve);
-
-
-            //------------- Orange ------------------
-            Property stJamesPlace = new Property("St. James Place", "Orange", 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16);
-            boardPositionToPropertyMap.Add(stJamesPlace.getBoardPosition(), stJamesPlace);
-
-            Property tennesseeAve = new Property("Tennessee Avenue", "Orange", 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 18);
-            boardPositionToPropertyMap.Add(tennesseeAve.getBoardPosition(), tennesseeAve);
-
-            Property newYorkAve = new Property("New York Avenue", "Orange", 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 19);
-            boardPositionToPropertyMap.Add(newYorkAve.getBoardPosition(), newYorkAve);
-
-
-            //------------- Red ------------------
-            Property kentuckyAve = new Property("Kentucky Avenue", "Red", 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 21);
-            boardPositionToPropertyMap.Add(kentuckyAve.getBoardPosition(), kentuckyAve);
-
-            Property indianaAve = new Property("Indiana Avenue", "Red", 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 23);
-            boardPositionToPropertyMap.Add(indianaAve.getBoardPosition(), indianaAve);
-
-            Property illinoisAve = new Property("Illinois Avenue", "Red", 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 24);
-            boardPositionToPropertyMap.Add(illinoisAve.getBoardPosition(), illinoisAve);
-
-
-            //------------- Yellow ------------------
-            Property atlanticAve = new Property("Atlantic Avenue", "Yellow", 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 26);
-            boardPositionToPropertyMap.Add(atlanticAve.getBoardPosition(), atlanticAve);
-
-            Property ventnorAve = new Property("Ventnor Avenue", "Yellow", 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 27);
-            boardPositionToPropertyMap.Add(ventnorAve.getBoardPosition(), ventnorAve);
-
-            Property marvinGardens = new Property("Marvin Gardens", "Yellow", 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 29);
-            boardPositionToPropertyMap.Add(marvinGardens.getBoardPosition(), marvinGardens);
-
-
-            //------------- Green ------------------
-            Property pacificAve = new Property("Pacific Avenue", "Green", 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 31);
-            boardPositionToPropertyMap.Add(pacificAve.getBoardPosition(), pacificAve);
-
-            Property northCarolinaAve = new Property("North Carolina Avenue", "Green", 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 32);
-            boardPositionToPropertyMap.Add(northCarolinaAve.getBoardPosition(), northCarolinaAve);
-
-            Property pennsylvaniaAve = new Property("Pennsylvania Avenue", "Green", 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 34);
-            boardPositionToPropertyMap.Add(pennsylvaniaAve.getBoardPosition(), pennsylvaniaAve);
-
-
-            //------------- Dark Blue ------------------
-            Property parkPlace = new Property("Park Place", "Dark Blue", 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 37);
-            boardPositionToPropertyMap.Add(parkPlace.getBoardPosition(), parkPlace);
-
-            Property boardwalk = new Property("Boardwalk", "Dark Blue", 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 39);
-            boardPositionToPropertyMap.Add(boardwalk.getBoardPosition(), boardwalk);
+            
         }
 
         private void propertyPanel_Click(object? sender, EventArgs e)
--- Property.cs
+++ Property.cs
@@ -9,39 +9,12 @@ namespace MonopolyGame
 {
     public class Property
     {
-        private String name;
-        private Player owner;
-        private String colorGroup;
-        private int colorGroupSize;
-        private int cost;
-        private int baseRent;
-        private int wholeGroupRent;
-        private int oneHouseRent;
-        private int twoHouseRent;
-        private int threeHouseRent;
-        private int fourHouseRent;
-        private int hotelRent;
-        private int housePrice;
-        private int houseCount;
-        private int boardPosition;
+        protected String name;
+        protected String type;
+        protected Player owner;
+        protected int cost;
+        protected int boardPosition;
 
-        public Property(String name, String ColorGroup, int colorGroupSize, int cost, int baseRent, int wholeGroupRent, int oneHouseRent, int twoHouseRent, int threeHouseRent, int fourHouseRent, int hotelRent, int housePrice, int boardPosition)
-        {
-            this.name = name;
-            this.colorGroup = ColorGroup;
-            this.colorGroupSize = colorGroupSize;
-            this.cost = cost;
-            this.baseRent = baseRent;
-            this.wholeGroupRent = wholeGroupRent;
-            this.oneHouseRent = oneHouseRent;
-            this.twoHouseRent = twoHouseRent;
-            this.threeHouseRent = threeHouseRent;
-            this.fourHouseRent = fourHouseRent;
-            this.hotelRent = hotelRent;
-            this.housePrice = housePrice;
-            this.houseCount = 0;
-            this.boardPosition = boardPosition;
-        }
         public String getName()
         {
             return name;
@@ -54,63 +27,39 @@ namespace MonopolyGame
         {
             this.owner = owner;
         }
-        public String getColorGroup()
-        {
-            return colorGroup;
-        }
         public int getCost()
         {
             return cost;
         }
-        public int getRent()
-        {
-            int colorGroupOwned = 0;
 
-            foreach (Property property in owner.getProperties())
-            {
-                if (property.getColorGroup() == colorGroup)
-                {
-                    colorGroupOwned++;
-                }
-            }
-            if (colorGroupOwned == colorGroupSize)
-            {
-                switch (houseCount)
-                {
-                    case 0:
-                        return wholeGroupRent;
-                    case 1:
-                        return oneHouseRent;
-                    case 2:
-                        return twoHouseRent;
-                    case 3:
-                        return threeHouseRent;
-                    case 4:
-                        return fourHouseRent;
-                    default:
-                        return wholeGroupRent;
-                }
-            }
-            else
-            {
-                return baseRent;
-            }
-        }
-        public int getHousePrice()
-        {
-            return housePrice;
-        }
-        public int getHouseCount()
-        {
-            return houseCount;
-        }
-        public void setHouseCount(int houseCount)
-        {
-            this.houseCount = houseCount;
-        }
         public int getBoardPosition()
         {
             return boardPosition;
+        }
+        public String getType()
+        {
+            return type;
+        }
+        public virtual String getColorGroup()
+        {
+            return "";
+        }
+        public virtual int getRent(int roll)
+        {
+            return 0;
+        }
+
+        public virtual int getHousePrice()
+        {
+            return 0;
+        }
+        public virtual int getHouseCount()
+        {
+            return 0;
+        }
+        public virtual void setHouseCount(int houseCount)
+        {
+            
         }
     }
 }
