0efe5881d29e2913d76f8049c1af855ccf7b33a8
bethanii <bethanihampton@gmail.com>
2024-05-01 20:10:21 -0500

    adding hotel ui

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -153,6 +153,16 @@ namespace MonopolyGame.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap hotel {
+            get {
+                object obj = ResourceManager.GetObject("hotel", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap house {
             get {
                 object obj = ResourceManager.GetObject("house", resourceCulture);
